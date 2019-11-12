7e48122935edcb5b00e17897f18862085eddb2d7
MarcoADeCSilva <marcoacsilva@live.com>
2019-11-11 22:50:26 -0300

    Email HTML

--- src/main/java/com/nelioalves/cursomc/services/PedidoService.java
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -71,7 +71,7 @@ public class PedidoService {
 		
 		itemPedidoRepository.save(obj.getItens());
 		//System.out.println(obj);
-		emailService.sendOrderConfirmationEmail(obj);
+		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
 
