cadbf433a8c86d6110e764e3410657d8025f49e5
MarcoADeCSilva <marcoacsilva@live.com>
2020-01-09 22:36:39 -0300

    Implementando autenticação e geração do token JWT

---
+++ src/main/java/com/nelioalves/cursomc/dto/CredenciasDTO.java
@@ -0,0 +1,26 @@
+package com.nelioalves.cursomc.dto;
+
+import java.io.Serializable;
+
+public class CredenciasDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String email;
+	private String senha;
+	
+	public CredenciasDTO(){		
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
