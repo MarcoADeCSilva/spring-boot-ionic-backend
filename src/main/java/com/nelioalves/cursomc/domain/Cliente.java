0b9758844db820dae2490dd15b45ad0e73183603
Marco Aurélio de Carvalho Silva <34280276+MarcoADeCSilva@users.noreply.github.com>
2020-01-22 13:51:20 -0300

    Salvando a URL da imagem em Cliente

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -54,6 +54,8 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
+	private String imagemUrl;
+	
 	public Cliente() {		
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -172,5 +174,13 @@ public class Cliente implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public String getImagemUrl() {
+		return imagemUrl;
+	}
+
+	public void setImagemUrl(String imagemUrl) {
+		this.imagemUrl = imagemUrl;
 	}
 }
