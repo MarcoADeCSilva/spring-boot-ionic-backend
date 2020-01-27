cb663344d748ffcdf3448b726cc46bb850863f7d
Marco Aurélio de Carvalho Silva <34280276+MarcoADeCSilva@users.noreply.github.com>
2020-01-27 16:28:01 -0300

    Gap@Marco-Eng MINGW64 /e/Spring Boot com Ionic/Projetos/cursomc (master) $ git add .
    Gap@Marco-Eng MINGW64 /e/Spring Boot com Ionic/Projetos/cursomc (master)
    $ git commit -m

--- src/main/java/com/nelioalves/cursomc/repositories/CidadeRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/CidadeRepository.java
@@ -1,6 +1,9 @@
 package com.nelioalves.cursomc.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.nelioalves.cursomc.domain.Cidade;
@@ -8,4 +11,6 @@ import com.nelioalves.cursomc.domain.Cidade;
 @Repository
 public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
 	
+	@Query("SELECT obj FROM Cidade obj WHERE obj.estado.id = :estadoId ORDER BY obj.nome")
+	public List<Cidade> findCidades(Integer estado_id);
 }
---
+++ src/main/java/com/nelioalves/cursomc/resources/EstadoResource.java
@@ -0,0 +1,29 @@
+package com.nelioalves.cursomc.resources;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.domain.Estado;
+import com.nelioalves.cursomc.dto.EstadoDTO;
+import com.nelioalves.cursomc.services.EstadoService;
+
+@RestController
+@RequestMapping(value="/estados")
+public class EstadoResource {
+
+	@Autowired
+	private EstadoService service;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<EstadoDTO>> findAll(){
+		List<Estado> list = service.findAll();
+		List<EstadoDTO> listDTo = list.stream().map(obj -> new EstadoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTo);
+	}
+}
