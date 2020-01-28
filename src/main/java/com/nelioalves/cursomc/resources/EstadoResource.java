b2eef3955f4caa61c44c55a0fbaa70f9c5f061cd
MarcoADeCSilva <marcoacsilva@live.com>
2020-01-27 22:38:07 -0300

    Endpoints para buscar estados e cidades

--- src/main/java/com/nelioalves/cursomc/resources/EstadoResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/EstadoResource.java
@@ -5,12 +5,16 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nelioalves.cursomc.domain.Cidade;
 import com.nelioalves.cursomc.domain.Estado;
+import com.nelioalves.cursomc.dto.CidadeDTO;
 import com.nelioalves.cursomc.dto.EstadoDTO;
+import com.nelioalves.cursomc.services.CidadeService;
 import com.nelioalves.cursomc.services.EstadoService;
 
 @RestController
@@ -20,10 +24,20 @@ public class EstadoResource {
 	@Autowired
 	private EstadoService service;
 	
+	@Autowired
+	private CidadeService cidadeService;
+	
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<EstadoDTO>> findAll(){
 		List<Estado> list = service.findAll();
 		List<EstadoDTO> listDTo = list.stream().map(obj -> new EstadoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTo);
+	}
+	
+	@RequestMapping(value="/{estadoId}/cidades", method=RequestMethod.GET)
+	public ResponseEntity<List<CidadeDTO>> findCidades(@PathVariable Integer estadoId){
+		List<Cidade> list = cidadeService.findByEstado(estadoId);
+		List<CidadeDTO> listDTo = list.stream().map(obj -> new CidadeDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTo);
 	}
 }
